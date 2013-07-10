324579597c6041dbb973c612f846a8d6422f87a6
Stefano Rivera <stefano@yola.com>
2013-07-10 16:01:41 +0200

    README and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='authkt',
+    version='0.0.1',
+    description='Python implementation of mod_auth_tkt cookies',
+    author='Yola',
+    author_email='engineers@yola.com',
+    url='http://github.com/yola/authtkt',
+    py_modules=['authtkt'],
+    test_suite='nose.collector',
+    install_requires=['M2Crypto < 1.0.0', 'yocommon < 2.0.0']
+)
