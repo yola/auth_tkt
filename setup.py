85cedfe774865ac177e959e2885a5a5d5c446dff
kosmos342 <kosmos342@gmail.com>
2024-03-04 06:08:13 +0200

    nosetests->unittest. Remove using deprecated testing via setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,5 @@ setup(
     author_email='engineers@yola.com',
     url='http://github.com/yola/auth_tkt',
     packages=['auth_tkt'],
-    test_suite='nose.collector',
     install_requires=['cryptography < 4.0']
 )
